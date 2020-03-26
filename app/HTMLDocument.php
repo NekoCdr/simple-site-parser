e06c0b50c831b5cf8da74f1a6a491a8dd0f7fb35
Yuri Istomin <intro1994@gmail.com>
2020-03-26 17:05:07 +0200

    Added HTMLDocument
    - Created HTMLDocument class
    - Added HTML document loader via curl
    - Updated composer requirements

---
+++ app/HTMLDocument.php
@@ -0,0 +1,67 @@
+<?php
+
+
+namespace SSP;
+
+
+/**
+ * Class HTMLDocument
+ * @package SSP
+ */
+class HTMLDocument extends \DOMDocument
+{
+	/**
+	 * @var string
+	 */
+	public string $document_url;
+
+	/**
+	 * @var string
+	 */
+	public string $requested_url;
+
+	/**
+	 * @var int
+	 */
+	public int $redirect_count;
+
+	/**
+	 * @var int
+	 */
+	public int $max_redirects;
+
+	/**
+	 * @var int
+	 */
+	public int $http_code;
+
+	/**
+	 * @param string $requested_url
+	 * @param int $max_redirects
+	 * @return bool
+	 */
+	public function loadCurlDocument(string $requested_url, int $max_redirects = 0): bool
+	{
+		$this->requested_url = $requested_url;
+		$this->max_redirects = $max_redirects;
+
+		$curl_data = curl_init($this->requested_url);
+		$curl_options = [
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_MAXREDIRS => $this->max_redirects
+		];
+		curl_setopt_array($curl_data, $curl_options);
+		$curl_response = curl_exec($curl_data);
+		$curl_get_info = curl_getinfo($curl_data);
+
+		$this->http_code = $curl_get_info['http_code'];
+		$this->redirect_count = $curl_get_info['redirect_count'];
+
+		if ($this->http_code == 200) {
+			$this->document_url = $curl_get_info['url'];
+			return $this->loadHTML($curl_response);
+		} else
+			return false;
+	}
+}
--- parse.php
+++ parse.php
@@ -1,5 +1,8 @@
 <?php
 
+use SSP\CliWrapper;
+use SSP\HTMLDocument;
+
 define('ROOT_PATH', __DIR__);
 
 require_once ROOT_PATH.'/vendor/autoload.php';
@@ -11,7 +14,15 @@ try {
 	 * r   - max HTTP redirects (0 - disable redirects)
 	 * n   - nesting level for recursive parsing (0 - infinity)
 	 */
-	$options = \SSP\CliWrapper::getOptions('r:n:', ['url:'], ['url'], ['r' => 10, 'n' => 0]);
+	$options = CliWrapper::getOptions('r:n:', ['url:'], ['url'], ['r' => 10, 'n' => 0]);
+
+	$doc = new HTMLDocument();
+	if (!$doc->loadCurlDocument($options['url'], $options['r'])) {
+		if ($doc->http_code == 308 && $doc->redirect_count == $doc->max_redirects)
+			throw new Exception("Document wasn't load. Too many redirects.");
+		else
+			throw new Exception("Document wasn't load. HTTP code: {$doc->http_code}");
+	}
 } catch (Exception $e) {
 	echo $e->getMessage(), PHP_EOL;
 	exit();
